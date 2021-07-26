8f04369e21325c1eb10941fd10a71b586688062b
Rudi Roman <roma.tcheloweck@gmail.com>
2021-07-27 00:09:57 +0300

    Optimised third problem. Solved without errors.

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,7 +1,3 @@
 function ucFirst(str) {
-  // let result = str.toUpperCase();
-  // if (str.length > 1) {
-    /*result =*/return str.slice(0, 1).toUpperCase() + str.slice(1);
-  // }
-  // return result;
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
