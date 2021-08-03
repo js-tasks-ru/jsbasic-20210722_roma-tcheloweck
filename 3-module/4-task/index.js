8de1c26ac33c890121fd737970e36a9f23e44ecc
Rudi Roman <roma.tcheloweck@gmail.com>
2021-08-03 11:35:40 +0300

    Solved 4 problem before testing

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filtered = users.filter(user => user.age <= age);
+
+  let message = '';
+  
+  for (const elem of filtered) {
+    message += `${elem['name']}, ${elem['balance']}\n`;
+  }
+
+  return message.slice(-1);
 }
