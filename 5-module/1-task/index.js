0a8059a739ccf1af26e2675fbc2a19816153ee63
Rudi Roman <roma.tcheloweck@gmail.com>
2021-08-10 18:30:44 +0300

    Solved 3 problems. Tested without errors.

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
-  // ваш код...
+  let button = document.querySelector('.hide-self-button');
+
+  function onHidden() {
+    button.setAttribute('hidden', '');
+  }
+
+  button.addEventListener('click', onHidden, { once: true });
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,16 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let textElement = document.getElementById('text');
+
+  if (!button || !textElement) return;
+
+  function toggleVisibility() {
+    if (textElement.hasAttribute('hidden')) {
+      textElement.removeAttribute('hidden');
+    } else {
+      textElement.setAttribute('hidden', '');
+    }
+  }
+
+  button.addEventListener('click', toggleVisibility);
 }
