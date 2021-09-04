d0d044232c1f3f212be58af5e00b5c80c2fb08ea
Rudi Roman <roma.tcheloweck@gmail.com>
2021-09-04 18:19:21 +0300

    Problem with await in task-2

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import Carousel from '../../6-module/3-task/index.js';
 import slides from '../../6-module/3-task/slides.js';
 
@@ -16,6 +18,28 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    return new Promise((resolve, reject) => {
+      let carousel = new Carousel(slides);
+      document.querySelector('[data-carousel-holder]').append(carousel.elem);
+
+      let ribbonMenu = new RibbonMenu(categories);
+      document.querySelector('[data-ribbon-holder]').append(ribbonMenu.elem);
+
+      let slider = new StepSlider(5, 3);
+      document.querySelector('[data-slider-holder]').append(slider.elem);
+
+      let cartIcon = new CartIcon();
+      document.querySelector('[data-cart-icon-holder]').append(cartIcon.elem);
+
+      let cart = new Cart(cartIcon);
+
+      let products = await fetch('products.json', { method: 'POST' })
+        .then(response => JSON.parse(response))
+        .catch(console.log('Ошибка при получении товаров с сервера!'));
+    
+      let grid = new ProductsGrid(products);
+      document.querySelector('[data-products-grid-holder]').append(grid.elem);
+
+    });
   }
 }
